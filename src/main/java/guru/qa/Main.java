f8611f167c9466ae725dbc4420493ec4bd9db1df
Alexander.Shestov <shestov.alex@gmail.com>
2022-07-30 00:13:57 +0200

    test commit

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -5,8 +5,8 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args){
         Integer year = LocalDate.now().getYear();
-        if (year > 2022)
-            System.out.println("2022 прошёл!");
+        if (year >= 2022 & year%4==0)
+            System.out.println("2022 прошёл!, да ешё сейчас високсоный год");
     }
 
 }
