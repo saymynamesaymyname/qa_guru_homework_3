e8f790a6630c85b5431a5d836651adca76d95a60
Alexander.Shestov <shestov.alex@gmail.com>
2022-07-30 00:04:15 +0200

    test commit

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,4 +1,12 @@
 package guru.qa;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args){
+        Integer year = LocalDate.now().getYear();
+        if (year > 2022)
+            System.out.println("2022 прошёл!");
+    }
+
 }
